509b32652c80a95e4d4ac88127ba9ee17f1a7dd3
R. Beltran <rbeltran8000@gmail.com>
2022-02-11 00:56:04 +0100

    version info added

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -11,5 +11,10 @@ namespace DevTools.Humankind.GUITools
         ///     Change REVISION value to force a reset of player preferences.
         /// </summary>
         public const int REVISION = 304;
+        public const string DISPLAY_NAME = "GUI Tools";
+        public const string FULL_DISPLAY_NAME = "HUMANKIND GUI Tools";
+        public const string MOD_IO_URL_TEXT = "humankind.mod.io/gui-tools";
+        public const string MOD_IO_URL = "https://humankind.mod.io/gui-tools";
+        
     }
 }
