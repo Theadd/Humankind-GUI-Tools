684cf923095b5cd5400ae15aa3ea4955149e9804
R. Beltran <rbeltran8000@gmail.com>
2022-11-01 02:33:44 +0100

    v2.1.0

--- PluginInfo.cs
+++ PluginInfo.cs
@@ -4,12 +4,12 @@
     {
         public const string PLUGIN_GUID = "DevTools.Humankind.GUITools";
         public const string PLUGIN_NAME = "GUITools";
-        public const string PLUGIN_VERSION = "2.0.0.0";
+        public const string PLUGIN_VERSION = "2.1.0.0";
 
         /// <summary>
         ///     Change REVISION value to force a reset of player preferences.
         /// </summary>
-        public const int REVISION = 314;
+        public const int REVISION = 316;
 
         public const string DISPLAY_NAME = "GUI Tools";
         public const string FULL_DISPLAY_NAME = "HUMANKIND GUI Tools";
